bb7591ec0461a334c2c1d47aa6bf90cd6d9b1cc8
onur engun <onurengun@outlook.com>
2021-01-20 01:18:42 +0300

    Add project files.

---
+++ yapiciMetotlar_Constructors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yapiciMetotlar_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kartno ve bakiyeye burada değer verilir=
+            bankaKarti kart = new bankaKarti(2000, "156489641");
+
+        }
+    }
+}
---
+++ yapiciMetotlar_Constructors/bankaKarti.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yapiciMetotlar_Constructors
+{
+    class bankaKarti
+    {
+        private string kartNo;
+        private double bakiye;
+        private double kullanilanKredi;
+
+        //Constructor  - Yapıcı metot
+        //geri dönüş türü olmaz
+        //metot adı ait olduğu sınıf adı ile aynı olamalıdır
+        //Tanımlanan isim (bakiye ve kartno) aynı isimle kullanılmak istenirse başına this. koyularak kullanılır.
+        //this. kullanılmazsa baş harfi büyük kullanılabilir.
+        public bankaKarti(double Bakiye, string kartNo)
+        {
+            this.kartNo = kartNo;
+            bakiye = Bakiye;
+        }
+
+    }
+}
